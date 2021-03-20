4695f73a685d1d4a681cec1bec2eeed7a7fab58f
Sukanya <sukanya@sukanyas-air.home>
2021-03-20 12:39:22 +0000

    home page automation

---
+++ src/test/java/flightbooking/NewTest.java
@@ -0,0 +1,19 @@
+package test.java.flightbooking;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  WebDriver driver;
+		 System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
+		 driver = new ChromeDriver();
+		 driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		 driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXTaction")).click();
+		 driver.findElement(By.xpath("//a[@value='BLR']")).click();
+		 driver.close();
+  }
+}
