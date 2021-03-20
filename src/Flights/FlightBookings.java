846fcecb0c72b91e17dcea0eef2c9d1b2d1cda40
rajeshsaikrishnavurukonda <12079681+rajeshsaikrishnavurukonda@users.noreply.github.com>
2021-03-20 11:11:20 +0000

    Update FlightBookings.java
    minor change

--- src/Flights/FlightBookings.java
+++ src/Flights/FlightBookings.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.support.ui.Select;
 
 public class FlightBookings {
 	//main function starts here
+	//I have changed
 	
 	 public static void main(String args[]) throws InterruptedException {
      WebDriver driver;
